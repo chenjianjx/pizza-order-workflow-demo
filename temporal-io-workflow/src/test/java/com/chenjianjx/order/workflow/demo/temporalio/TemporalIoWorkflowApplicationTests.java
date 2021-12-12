f214599ada8689f4d3f9d0513af2cc00b70a5992
chenjianjx@gmail.com <chenjianjx@gmail.com>
2021-12-12 11:10:45 +1100

    create the module with spring-boot

---
+++ temporal-io-workflow/src/test/java/com/chenjianjx/order/workflow/demo/temporalio/TemporalIoWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenjianjx.order.workflow.demo.temporalio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalIoWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
